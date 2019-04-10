341ed3510810739dc0b3d8110234196f61cd1e2f
Whaylon Coleman <it@anitconsultant.com>
2019-04-10 10:36:24 -0500

    Initial commit (part 2).

---
+++ ZenMagApp/ZenMagApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ZenMagApp.Models;
+
+namespace ZenMagApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
